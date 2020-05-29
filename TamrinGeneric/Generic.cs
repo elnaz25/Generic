d6011df4c47e98a953790dc2cc39f01e16deea47
elnaz25 <elnazjangi9224@gmail.com>
2020-05-29 16:43:55 +0430

    Add project files.

---
+++ TamrinGeneric/Generic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace TamrinGeneric
+{
+    public class GenericClass<T> where T : class , new ()
+    {
+
+        public object ReadFileToObject(string fileName)
+        {
+
+            var type = typeof(T);
+            var obj = Activator.CreateInstance(type);
+            var result = obj;
+            string[] lines = File.ReadAllLines(fileName);
+            PropertyInfo[] props = obj.GetType().GetProperties();
+
+            foreach (var prop in props)
+            {
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string propName = lines[i].Split(':')[0].Trim();
+                    string value = lines[i].Split(':')[1].Trim();
+
+                    if (prop.Name.ToLower() == propName.ToLower())
+                    {
+                        prop.SetValue(obj, Convert.ChangeType(value, prop.PropertyType), null);
+                        break;
+                    }
+                }
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ TamrinGeneric/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace TamrinGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var genericclass = new GenericClass<Person>();
+            string textpath = Path.Combine(Environment.CurrentDirectory, "TextFile.txt");
+
+            genericclass.ReadFileToObject(textpath);
+        }
+    }
+}
